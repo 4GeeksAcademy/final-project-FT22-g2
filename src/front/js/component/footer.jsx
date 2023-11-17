bbebc06d96b43f16a21650cdee837b36fa5a18e1
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-11-17 14:09:04 +0000

    esta ordenado

--- src/front/js/component/footer.jsx
+++ src/front/js/component/footer.jsx
@@ -2,55 +2,70 @@ import React, { Component } from "react";
 import logoElRinconDelVino from "../../img/logoElRinconDelVino.jpg";
 import webpayLogo from "../../img/webpayLogo.png";
 import consumoResponsableLogo from "../../img/consumoResponsableLogo.png";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 
 
 
 const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center" style={{ backgroundColor: "#7B2121" }}>
 
-		<div className="container p-2" style={{ backgroundColor: "#7B2121", height: "250px" }} >
-			<div className="row">
+		<div className="container-fluid p-2" style={{ maxWidth: "100%", margin: "0" }} >
+			<div className="row align-items-start">
+
 				{/* logo */}
-				<div className="col-lg-4" img-lg-start>
-					<img src={logoElRinconDelVino} className="img-fluid rounded" alt="logo" />
+
+				<div className="col-lg-4" align-self-start>
+					<img src={logoElRinconDelVino} className="img-fluid rounded" alt="logo" style={{ maxHeight: "350px" }} />
 				</div>
+
 				{/* Corporativo */}
-				<div className="col-lg-4" >
-					<h5>Corporativo</h5>
-					<ul style={{ listStyleType: "none", padding: 0 }}>
-						<li>Términos y condiciones</li>
-						<li>Politicas de despacho</li>
-						<li>Cambios y devoluciones</li>
+
+				<div className="col-lg-4 p-3" >
+					<h6 style={{ color: "white" }}>Corporativo</h6>
+					<ul style={{ listStyleType: "none", padding: 0, color: "white" }}>
+						<li><a href="#" style={{ color: "white", textDecoration: "none" }}>Términos y condiciones</a></li>
+						<li><a href="#" style={{ color: "white", textDecoration: "none" }}>Políticas de despacho</a></li>
+						<li><a href="#" style={{ color: "white", textDecoration: "none" }}>Cambios y devoluciones</a></li>
 					</ul>
 
 					{/* Contactanos */}
-					<div className="...">
-						<h6>Contáctanos</h6>
-						<p>elrincondelvino@gmail.com</p>
-						<img src="..." alt="logo instagram" />
-						<img src="..." alt="logo facebook" />
+
+					<div className="mt-5 p-4">
+						<h6 style={{ color: "white" }}>Contáctanos</h6>
+						<p><a href="mailto:elrincondelvino@gmail.com" style={{ color: "white", textDecoration: "none" }}>elrincondelvino@gmail.com</a></p>
+
+
+						{/* Íconos de redes sociales */}
+
+						<FontAwesomeIcon icon={faInstagram} size="3x" style={{ color: "white", marginLeft: "10px" }} />
+						<FontAwesomeIcon icon={faFacebookSquare} size="3x" style={{ color: "white", marginLeft: "10px" }} />
+
 					</div>
 				</div>
 
 				{/* Medios de pago */}
-				<div className="col-lg-4 ">
-					<h5>Medios de pago</h5>
+
+				<div className="col-lg-4 p-3">
+					<h6 style={{ color: "white" }}>Medios de pago</h6>
 					<div className="row">
 						<div className="col-lg-12 mb-3">
 							<img src={webpayLogo} style={{ width: "50%" }} alt="logo tarjeta visa" />
 						</div>
-						<div col-lg-12 mb-3 >
-						<img src={consumoResponsableLogo} style={{ width: "49%" }} alt="logo consumo responsable" />
-						<p><strong>#SiBebesNoConduzcas</strong></p>
-						<p><strong>#PasaLasLlaves</strong></p>
+
+						{/* Consumo Responsable */}
+
+						<div className="col-md-12 d-flex flex-column align-items-center mt-4">
+							<img src={consumoResponsableLogo} style={{ width: "49%", marginBottom: "15px"}} alt="logo consumo responsable" />
+							<p style={{ color: "white", marginBottom: "5px" }}>#SiBebesNoConduzcas</p>
+							<p style={{ color: "white" }}>#PasaLasLlaves</p>
 						</div>
 					</div>
-					</div>
-
 				</div>
 
 			</div>
+
+		</div>
 	</footer>
 );
 
