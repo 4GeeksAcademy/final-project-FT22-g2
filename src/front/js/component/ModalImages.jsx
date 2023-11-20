7627e619639b803f4256b23ae1a79918dfbb2cfc
Matías Rivas <87586371+Dageti@users.noreply.github.com>
2023-11-20 12:35:09 +0000

    correcciones a modal de producto, falta corregir dimensiones

--- src/front/js/component/ModalImages.jsx
+++ src/front/js/component/ModalImages.jsx
@@ -10,7 +10,7 @@ const ModalImages = () => {
 
             {/* <!-- Modal --> */}
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-lg">
                     <div class="modal-content">
 
                         <div class="modal-body container">
@@ -24,7 +24,7 @@ const ModalImages = () => {
                                 </div>
                                 <div className="col-7 mt-3">
                                     <h5>Nombre Producto</h5>
-                                    <div className="my-3">
+                                    <div className="my-5">
                                         <i class="fa-solid fa-star stars"></i>
                                         <i class="fa-regular fa-star"></i>
                                         <i class="fa-regular fa-star"></i>
@@ -33,7 +33,7 @@ const ModalImages = () => {
                                     </div>
                                     Precio: $10.000
                                     <div>
-                                        <button type="button" class="btn btn-secondary mt-4">Secondary</button>
+                                        <button type="button" class="btn btn-secondary mt-5">Secondary</button>
                                     </div>
 
                                 </div>
