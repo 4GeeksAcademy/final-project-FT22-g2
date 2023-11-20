78fc61c333cebf043c8ed0769b53bb2f536cd750
Jorge Oteiza <oteiza.jor@gmail.com>
2023-11-20 04:52:55 +0000

    Estructura visualización detallada en proceso

---
+++ src/front/js/component/ProductCard.jsx
@@ -0,0 +1,25 @@
+// ProductCard.jsx
+import React from 'react';
+import PropTypes from "prop-types";
+
+const ProductCard = ({ name, price, imageUrl }) => (
+    <div className="card" style={{ width: "18rem", borderRadius: "15px" }}>
+        <img className="card-img-top" src={imageUrl} alt="Card image cap" />
+        <div className="card-body">
+            <h4 className="card-title">{name}</h4>
+            <h5>Precio: {price}</h5>
+            {/* ... Otros detalles del producto */}
+            <button className="btn btn-light rounded-pill">
+                Agregar al carrito
+            </button>
+        </div>
+    </div>
+);
+
+ProductCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+};
+
+export default ProductCard;
--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -1,16 +1,45 @@
-import React, { useState, useEffect, useContext } from "react";
+// single.js
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import ProductCard from "../components/ProductCard";
 
-export const Single = (props) => {
+const Single = (props) => {
   const { store, actions } = useContext(Context);
   const params = useParams();
 
+  const product = {
+    name: "Nombre de vino",
+    price: 20,
+    imageUrl: "https://dummyimage.com/100x300/000/fff",
+    stars: 5,
+  };
+
   return (
-    <div className="jumbotron">
-      <h1>espacio de trabajo</h1>
+    <div className="container">
+      {/* ... Código restante */}
+      <div className="row my-5">
+        <div className="col-md-6">
+          <img
+            className="card-img-top"
+            src={product.imageUrl}
+            alt="Card image cap"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="card-title">{product.name}</h4>
+          <p className="card-text">
+            {Array.from({ length: product.stars }, (_, index) => (
+              <i key={index} className="fa-solid fa-star stars"></i>
+            ))}
+          </p>
+          <button className="btn btn-light rounded-pill my-2">
+            Agregar al carrito
+          </button>
+        </div>
+      </div>
+      {/* ... Más código */}
     </div>
   );
 };
@@ -18,3 +47,5 @@ export const Single = (props) => {
 Single.propTypes = {
   match: PropTypes.object,
 };
+
+export default Single.js;
