7d2831577e7fa798dad256b1c4218d3dc97835f8
Jorge Oteiza <oteiza.jor@gmail.com>
2023-11-21 17:31:47 +0000

    centrando y ordenando

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -1,40 +1,47 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { Context } from "../store/appContext.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import ProductCard from "../component/ProductCard.jsx";
+import "../../styles/ProductCard.css";
 
 const Single = () => {
   const { store } = useContext(Context);
   const { name, price, imageUrl, stars } = store.product || {};
 
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="row my-5">
-        <div className="col-md-6 custom-center">
-          <ProductCard name={name} price={price} imageUrl={imageUrl} />
+        <div className="col-md-6 col-xs-12 custom-center">
+          <ProductCard imageUrl={imageUrl} />
         </div>
-        <div className="col-md-6">
-          <div className="text-center my-4">
+        <div className="col-md-6 col-xs-12">
+          <div className="mb-4 mx-3 mx-md-5">
             <h2>Nombre de vino</h2>
-            <p className="card-text">
-              <i class="fas fa-star stars"></i>
-              <i class="far fa-star stars"></i>
-              <i class="far fa-star stars"></i>
-              <i class="far fa-star stars"></i>
-              <i class="far fa-star stars"></i>
+            <p className="card-text text-start">
+              <i className="fas fa-star stars"></i>
+              <i className="far fa-star stars"></i>
+              <i className="far fa-star stars"></i>
+              <i className="far fa-star stars"></i>
+              <i className="far fa-star stars"></i>
             </p>
-            <p className="text-black">750cc</p>
+            <p className="text-secondary mt-1 mb-5">750cc</p>
+            <p className="px-2 price-carrito-hover text-black">
+              Precio: 40.000
+            </p>
             <div className="product-price-carrito-hover d-inline-flex align-items-center justify-content-between">
-              <button className="button-add-remove-carrito-hover remove-carrito-hover bg-white">
+              <button className="button-add-remove-carrito-hover remove-carrito-hover bg-white mx-3">
                 -
               </button>
-              <label className="label-cantidad-carrito-hover px-2">1</label>
-              <button className="button-add-remove-carrito-hover add-carrito-hover bg-white">
+              <label className="label-cantidad-carrito-hover px-2 mx-3">
+                1
+              </label>
+              <button className="button-add-remove-carrito-hover add-carrito-hover bg-white mx-3">
                 +
               </button>
-              <p className="px-2 price-carrito-hover">40.000</p>
             </div>
-            <button className="btn btn-light rounded-pill my-2">
+            <button className="btn btn-secondary rounded-pill my-2 px-4">
               Agregar al carrito
             </button>
           </div>
@@ -51,10 +58,10 @@ const Single = () => {
 
       {/* Sección de Descripción del producto */}
       <div className="text-center my-4">
-        <h3 className="bg-black text-white d-inline-block p-2">
+        <h3 className="bg-black text-white d-inline-block p-2 rounded rounded-lg">
           Descripción del Producto
         </h3>
-        <div class="ps-relative">
+        <div className="ps-relative">
           <textarea
             id="wmd-input"
             name="post-text"
@@ -73,19 +80,66 @@ const Single = () => {
 
       {/* Sección de Reseñas del producto */}
       <div className="text-center my-4">
-        <h3 className="bg-black text-white d-inline-block p-2">
+        <h3 className="bg-black text-white d-inline-block p-2 rounded rounded-lg">
           Reseñas del Producto
         </h3>
-        <div className="calificaciones d-flex">
-          <h1 className="text-left">5</h1>
-          <p class="stars">
-            <i class="fas fa-star stars"></i>
-            <i class="far fa-star stars"></i>
-            <i class="far fa-star stars"></i>
-            <i class="far fa-star stars"></i>
-            <i class="far fa-star stars"></i>
+        <div className="calificaciones d-flex align-items-center">
+          <h2 className="text-left">5</h2>
+          <p className="stars ml-2">
+            <i className="fas fa-star stars"></i>
+            <i className="far fa-star stars"></i>
+            <i className="far fa-star stars"></i>
+            <i className="far fa-star stars"></i>
+            <i className="far fa-star stars"></i>
           </p>
-          <p className="text-black">sin calificaciones</p>
+          {/** Condición para mostrar "sin calificaciones" */}
+          {typeof stars === "number" && stars === 0 ? (
+            <p className="text-black ml-2">68 calificaciones</p>
+          ) : null}
+        </div>
+      </div>
+
+      <div className="comment mt-3 mb-3">
+        <div className="stars ml-2">
+          <i className="fas fa-star stars"></i>
+          <i className="far fa-star stars"></i>
+          <i className="far fa-star stars"></i>
+          <i className="far fa-star stars"></i>
+          <i className="far fa-star stars"></i>
+        </div>
+        <span className="ml-2">13 Nov. 2023</span>
+        <div className="mt-2 mb-2">
+          <p className="text-secondary">
+            Estaba maravilloso exquisito arte, 5 estrellas.
+            <button>
+              <FontAwesomeIcon icon="fa-regular fa-thumbs-up" />
+            </button>
+            <button>
+              <FontAwesomeIcon icon="fa-regular fa-thumbs-down" />
+            </button>
+          </p>
+        </div>
+      </div>
+
+      <div className="comment mt-3 mb-3">
+        <div className="stars ml-2">
+          <i className="fas fa-star stars"></i>
+          <i className="far fa-star stars"></i>
+          <i className="far fa-star stars"></i>
+          <i className="far fa-star stars"></i>
+          <i className="far fa-star stars"></i>
+        </div>
+        <span className="ml-2">13 Nov. 2023</span>
+        <div className="mt-2 mb-2">
+          <p className="text-secondary">
+            Espectacular, PERO.
+            <button>
+              <FontAwesomeIcon icon="fa-regular fa-thumbs-up" />
+            </button>
+            <button>
+              <FontAwesomeIcon icon="fa-regular fa-thumbs-down" />
+            </button>
+          </p>
         </div>
       </div>
     </div>
