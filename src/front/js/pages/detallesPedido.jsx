9af55a548f2952e40c4e8b6ec8ce961ac7c2ec00
Jorge Oteiza <oteiza.jor@gmail.com>
2023-11-22 15:27:35 +0000

    arreglar error de consola pay

---
+++ src/front/js/pages/detallesPedido.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import OrderDetails from '../components/OrderDetails';
+
+const DetallesPedido = () => {
+    return (
+        <div>
+            {/* ... Otro contenido de la página ... */}
+            <OrderDetails />
+        </div>
+    );
+};
+
+export default DetallesPedido;
