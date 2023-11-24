ea028327d02623f515f0cb5713f54441fbc82788
Demian Sotomayor <122990398+Demian-Sotomayor@users.noreply.github.com>
2023-11-24 13:17:01 +0000

    Prueba Demian entender backend

--- migrations/versions/08dfd04d89d6_.py
+++ migrations/versions/08dfd04d89d6_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 646d8facc49a
+Revision ID: 08dfd04d89d6
 Revises: 
-Create Date: 2023-11-16 18:29:07.827554
+Create Date: 2023-11-23 21:00:44.356347
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '646d8facc49a'
+revision = '08dfd04d89d6'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# TABLAS DE USUARIO
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    lastName = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    userLogin = db.relationship("UserLogin", backref="user", uselist=False)
+    historialLogin = db.relationship("HistorialLogin", backref="user", uselist=False)
+    historialPassword = db.relationship("HistorialPassword", backref="user", uselist=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +21,30 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "lastName": self.lastName
             # do not serialize the password, its a security breach
         }
+
+class UserLogin(db.Model):
+    __tablename__ = "UserLogin"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+class HistorialLogin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+class HistorialPassword(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+# TABLAS DE PRODUCTOS Y CARRITO
