5a504276a43bf9ab64a972d8e62377a0e8db1a81
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-11-24 12:46:16 +0000

    commit de prueba para subir cambios a mi rama de prueba

--- src/api/models.py
+++ src/api/models.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    direccion = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +20,35 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class UsuarioLogin(db.Model):
+    __tablename__ = "UserLogin"  
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    login_exitoso = db.Column(db.Boolean, nullable=False)
+    userLogin = db.relationship("UserLogin", backref="users", uselist=False)
+    historialLogin = db.relationship("HistorialLogin", backref="users", uselist=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "lastName": self.lastName
+            # do not serialize the password, its a security breach
+        }
+
+
+class HistorialLogin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+
