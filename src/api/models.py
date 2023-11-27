15c951a503418d026308e83fbfd8b7f8d6c19653
Demian Sotomayor <122990398+Demian-Sotomayor@users.noreply.github.com>
2023-11-27 20:29:45 +0000

    Siguiendo con las tablas

--- src/api/models.py
+++ src/api/models.py
@@ -25,11 +25,63 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            "nombre": self.nombre,
-            "lastName": self.lastName
-            # do not serialize the password, its a security breach
+            "active": self.active,
+            "last_login": self.last_login,
         }
+
+    def serialize_with_profile(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "active": self.active,
+            "last_login": self.last_login,
+            "profile": self.profile.serialize()
+        }
+
+    def serialize_with_favoritos(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "active": self.active,
+            "last_login": self.last_login,
+            "favoritos": self.favoritos.serialize()
+        }
+
+    def serialize_with_facturas(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "active": self.active,
+            "last_login": self.last_login,
+            "facturas": self.facturas.serialize()
+        }
+
+    def serialize_with_ordenes(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "active": self.active,
+            "last_login": self.last_login,
+            "ordenes": self.ordenes.serialize()
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 
 class Profile(db.Model):
     __tablename__ = "profiles"
@@ -39,6 +91,23 @@ class Profile(db.Model):
     # Conexiones
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "avatar": self.avatar,
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 class Favorito(db.Model):
     __tablename__ = "favoritos"
     # Información de columnas
@@ -46,6 +115,22 @@ class Favorito(db.Model):
     # Conexiones
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     producto_id = db.Column(db.Integer, db.ForeignKey("productos.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 class Factura(db.Model):
     __tablename__ = "facturas"
@@ -62,6 +147,29 @@ class Factura(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     orden_id = db.Column(db.Integer, db.ForeignKey("ordenes.id"), nullable=False)
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "numero_factura": self.numero_factura,
+            "date": self.date,
+            "total": self.total,
+            "status": self.status,
+            "ref": self.ref,
+            "date_payed": self.date_payed,
+            "type_payment": self.type_payment,
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 class Orden(db.Model):
     __tablename__ = 'ordenes'
     id = db.Column(db.Integer, primary_key=True)
@@ -71,6 +179,34 @@ class Orden(db.Model):
     # Conexiones
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     factura = db.relationship("Factura", backref="orden", uselist=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "fecha": self.fecha,
+            "total": self.total,
+            "status": self.status,
+        }
+
+    def serialize_with_factura(self):
+        return {
+            "id": self.id,
+            "fecha": self.fecha,
+            "total": self.total,
+            "status": self.status,
+            "factura": self.factura
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 class Producto(db.Model):
     __tablename__ = 'productos'
@@ -87,6 +223,68 @@ class Producto(db.Model):
     ordenesProductos = db.relationship("OrdenProducto", backref="producto")
     facturasProductos = db.relationship("FacturaProducto", backref="producto")
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "categoria": self.categoria,
+            "tipo": self.tipo,
+            "unitFormat": self.unitFormat,
+            "precio": self.precio,
+            "active": self.active,
+            "image": self.image,
+        }
+
+    def serialize_with_favoritos(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "categoria": self.categoria,
+            "tipo": self.tipo,
+            "unitFormat": self.unitFormat,
+            "precio": self.precio,
+            "active": self.active,
+            "image": self.image,
+            "favoritos": self.favoritos.serialize()
+        }
+
+    def serialize_with_ordenesProductos(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "categoria": self.categoria,
+            "tipo": self.tipo,
+            "unitFormat": self.unitFormat,
+            "precio": self.precio,
+            "active": self.active,
+            "image": self.image,
+            "ordenesProductos": self.ordenesProductos.serialize()
+        }
+
+    def serialize_with_facturasProductos(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "categoria": self.categoria,
+            "tipo": self.tipo,
+            "unitFormat": self.unitFormat,
+            "precio": self.precio,
+            "active": self.active,
+            "image": self.image,
+            "facturasProductos": self.facturasProductos.serialize()
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 class OrdenProducto(db.Model):
     __tablename__ = 'ordenes_productos'
     id = db.Column(db.Integer, primary_key=True)
@@ -96,6 +294,24 @@ class OrdenProducto(db.Model):
     orden_id = db.Column(db.Integer, db.ForeignKey("ordenes.id"), nullable=False)
     producto_id = db.Column(db.Integer, db.ForeignKey("productos.id"), nullable=False)
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "cantidad": self.nombre,
+            "precio": self.categoria,
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 class FacturaProducto(db.Model):
     __tablename__ = 'facturas_productos'
     id = db.Column(db.Integer, primary_key=True)
@@ -104,3 +320,21 @@ class FacturaProducto(db.Model):
     # Conexiones
     factura_id = db.Column(db.Integer, db.ForeignKey("facturas.id"), nullable=False)
     producto_id = db.Column(db.Integer, db.ForeignKey("productos.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "cantidad": self.nombre,
+            "precio": self.categoria,
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
