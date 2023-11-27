ad84f2c4c44abe9642f56c54b849f28d8c3c4f59
Demian Sotomayor <122990398+Demian-Sotomayor@users.noreply.github.com>
2023-11-27 18:20:31 +0000

    Agregando rutas y retocando estilos

--- src/front/js/pages/metodoDePago.jsx
+++ src/front/js/pages/metodoDePago.jsx
@@ -24,8 +24,8 @@ const MetodoDePago = () => {
 
             {/* TITULO Y BOTÓN PARA REGRESAR A VISTA ANTERIOR */}
             <div className="title-regresar-anterior-vista row">
-                <Link to="/detalles-pedido" className="text-decoration-none">
-                    <button type="button" className="button-regresar-anterior-vista col-1"><i class="fa-solid fa-arrow-left"></i></button>
+                <Link to="/detalles-pedido" className="text-decoration-none col-1">
+                    <button type="button" className="button-regresar-anterior-vista"><i class="fa-solid fa-arrow-left"></i></button>
                 </Link>
                 <h3 className="col-10">Método de pago</h3>
             </div>
--- src/front/js/pages/metodoDePagoRevisar.jsx
+++ src/front/js/pages/metodoDePagoRevisar.jsx
@@ -21,8 +21,8 @@ const MetodoDePagoRevisar = () => {
             {/* TITULO Y BOTÓN PARA REGRESAR A VISTA ANTERIOR */}
             <div className="title-regresar-anterior-vista row">
                 {/* VISTA PREVIA */}
-                <Link to={"/metodo-de-pago"} className="flecha-vista-anterior">
-                    <button type="button" className="button-regresar-anterior-vista col-1"><i class="fa-solid fa-arrow-left"></i></button>
+                <Link to={"/metodo-de-pago"} className="flecha-vista-anterior col-1">
+                    <button type="button" className="button-regresar-anterior-vista"><i class="fa-solid fa-arrow-left"></i></button>
                 </Link>
                 <h3 className="col-10">Confirmar compra</h3>
             </div>
