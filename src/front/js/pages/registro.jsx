066921eb115601ee3525d84d6c29d0dfaec672f2
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-11-27 19:59:25 +0000

    validacion de campos en registro listo

--- src/front/js/pages/registro.jsx
+++ src/front/js/pages/registro.jsx
@@ -10,24 +10,26 @@ const Registro = () => {
                         <div className="card-informacion-registro">
                             <h2>Crear cuenta</h2>
 
-                            <div className="row container-inputs-registro">
-                                <input type="text" placeholder='Nombre' className='input-registro' required />
-                                <input type="text" placeholder='Apellido' className='input-registro' required />
-                                <input type="email" placeholder='Email' className='input-registro' required />
-                                <input type="password" placeholder='Contraseña' className='input-registro' required />
-                            </div>
+                            <form className='needs-validation' novalidate>
+                                <div className="container-inputs-registro">
+                                    <input type="text" placeholder='Nombre' className="form-control input-registro" required />
+                                    <input type="text" placeholder='Apellido' className="form-control input-registro" required />
+                                    <input type="email" placeholder='Email' className="form-control input-registro" required />
+                                    <input type="password" placeholder='Contraseña' className="form-control input-registro" required />
+                                </div>
 
-                            <div className="container-terminos-y-condiciones">
-                                <input type="checkbox" name="terminos" id="terminosYcondiciones" required />
-                                <label className='terminosYcondicionesLabel'>Acepto los términos y condiciones</label>
-                            </div>
+                                <div className="container-terminos-y-condiciones">
+                                    <input type="checkbox" name="terminos" id="terminosYcondiciones" className='form-check-input'required />
+                                    <label className='terminosYcondicionesLabel'>Acepto los términos y condiciones</label>
+                                </div>
 
-                            <div className="container-btn-registro">
-                                <button type='button' className='btn btn-dark w-50'>Registrarme</button>
-                            </div>
+                                <div className="container-btn-registro">
+                                    <button type='submit' className='btn btn-dark w-50'>Registrarme</button>
+                                </div>
+                            </form>
 
                             <div className="container-btn-registro-google">
-                                <button className="btn btn-danger w-75" type="button" value="Login"disabled>(Próximamente) Continúa con <i class="fab fa-google me-2"></i></button>
+                                <button className="btn btn-danger w-75" type="button" value="Login" disabled>Continúa con <i class="fab fa-google me-2"></i>(Próximamente)</button>
                             </div>
                         </div>
                     </div>
