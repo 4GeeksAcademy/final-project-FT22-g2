c236a8161b989a6d7813b8e5ae0961cc22758faa
Demian Sotomayor <122990398+Demian-Sotomayor@users.noreply.github.com>
2023-11-28 15:10:41 +0000

    commit

--- src/front/js/pages/registro.jsx
+++ src/front/js/pages/registro.jsx
@@ -1,15 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "../../styles/registro.css";
 
 const Registro = () => {
-
-    const [formData, setFormData] = useState({})
-    const [formStatus, setFormStatus] = useState()
-
-    const onSubmit = (event) => {
-        setFormStatus("submitted")
-    }
-
     return (
         <>
             <div className="container-registro">
@@ -18,33 +10,27 @@ const Registro = () => {
                         <div className="card-informacion-registro">
                             <h2>Crear cuenta</h2>
 
-                            <form onSubmit={onSubmit} noValidate>
-
-
-                                <div className="row container-inputs-registro">
-                                    <input type="text" placeholder='Nombre' className='input-registro' required onChange={(event) => setFormData({ ...formData, name: event.target.value })} />
-                                    {
-                                        formStatus === "submitted" && !formData.name ? <p className='text-warning'>campo requerido</p> : <></>
-                                    }
-
-                                    <input type="text" placeholder='Apellido' className='input-registro' required />
-                                    <input type="email" placeholder='Email' className='input-registro' required />
-                                    <input type="password" placeholder='Contraseña' className='input-registro' required />
+                            <form className='needs-validation' novalidate>
+                                <div className="container-inputs-registro">
+                                    <input type="text" placeholder='Nombre' className="form-control input-registro" required />
+                                    <input type="text" placeholder='Apellido' className="form-control input-registro" required />
+                                    <input type="email" placeholder='Email' className="form-control input-registro" required />
+                                    <input type="text" placeholder='Contraseña' className="form-control input-registro" required />                                   
                                 </div>
 
                                 <div className="container-terminos-y-condiciones">
-                                    <input type="checkbox" name="terminos" id="terminosYcondiciones" required />
+                                    <input type="checkbox" name="terminos" id="terminosYcondiciones" className='form-check-input'required />
                                     <label className='terminosYcondicionesLabel'>Acepto los términos y condiciones</label>
                                 </div>
 
                                 <div className="container-btn-registro">
-                                    <button type="submit" className='btn btn-secondary w-50'>Registrarme</button>
-                                </div>
-
-                                <div className="container-btn-registro-google">
-                                    <button className="btn btn-danger w-75" value="Login"> Continúa con <i class="fab fa-google me-2"></i></button>
+                                    <button type='submit' className='btn btn-dark w-75'>Registrarme</button>
                                 </div>
                             </form>
+
+                            <div className="container-btn-registro-google">
+                                <button className="btn btn-danger w-75" type="button" value="Login" disabled>Continúa con <i class="fab fa-google me-2"></i>(Próximamente)</button>
+                            </div>
                         </div>
                     </div>
                 </div>
