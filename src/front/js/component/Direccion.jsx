2a5b8f5c9fa2072e62bac76f81a19ec8e63e7114
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-11-28 14:09:31 +0000

    validacion de direccion lista

--- src/front/js/component/Direccion.jsx
+++ src/front/js/component/Direccion.jsx
@@ -4,24 +4,20 @@ import "../../styles/cambiarDireccion.css";
 
 const Direccion = () => {
 
-   
-    
-
-
     return (
         <>
 
             {/* formulario */}
 
             <div className="card d-flex">
-                <div className="card-body" style={{ borderRadius: '10px', boxShadow: '0 0 10px #dadada'}}> 
+                <div className="card-body" style={{ borderRadius: '10px', boxShadow: '0 0 10px #dadada' }}>
 
                     <form className="needs-validation" novalidate>
                         <div className="row mb-4">
                             <div className="col-6">
                                 {/*  Region */}
                                 <label for="formGroupExampleInput" className="form-label">Región</label>
-                                <select type="text" className="form-select" placeholder="Región" aria-label="Región"required>
+                                <select type="text" className="form-select" placeholder="Región" aria-label="Región" required>
                                     <option selected>Selecciona tu región</option>
                                     <option value="1">Región de Arica y Parinacota</option>
                                     <option value="2">Región de Tarapacá</option>
@@ -44,7 +40,7 @@ const Direccion = () => {
                             {/* comuna */}
                             <div class="col-6">
                                 <label for="inputComuna" className="form-label">Comuna</label>
-                                <input type="text" className="form-control" placeholder="Comuna" aria-label="Comuna" id="comuna" required/>
+                                <input type="text" className="form-control" placeholder="Comuna" aria-label="Comuna" id="comuna" required />
                             </div>
                             <div className="invalid-feedback" id="comuna"></div>
                         </div>
@@ -52,26 +48,26 @@ const Direccion = () => {
                             {/* calle */}
                             <div class="col-6">
                                 <label for="inputCalle" className="form-label">Calle</label>
-                                <input type="text" className="form-control" placeholder="Calle" aria-label="Calle" id="calle"required />
+                                <input type="text" className="form-control" placeholder="Calle" aria-label="Calle" id="calle" required />
                                 <div className="invalid-feedback" id="calle"></div>
                             </div>
                             {/* numero de casa */}
                             <div class="col-6">
                                 <label for="inputNumero" className="form-label">Número</label>
-                                <input type="number" className="form-control" placeholder="N° de casa / dpto" aria-label="Número de casa" min="1" id="numero"required />
+                                <input type="number" className="form-control" placeholder="N° de casa / dpto" aria-label="Número de casa" min="1" id="numero" required />
                             </div>
-                             <div className="invalid-feedback" id="numero"></div>
+                            <div className="invalid-feedback" id="numero"></div>
                         </div>
                         <div class="row mb-4">
                             {/* codigo postal */}
                             <div class="col-6">
                                 <label for="finputCodigoPostal" className="form-label">Código postal</label>
-                                <input type="text" className="form-control" placeholder="código postal" aria-label="Código postal" required/>
+                                <input type="text" className="form-control" placeholder="código postal" aria-label="Código postal" required />
                             </div>
                             {/* Número de contacto */}
                             <div class="col-6">
                                 <label for="finputTelefono" className="form-label">Número de contacto</label>
-                                <input type="text" className="form-control" placeholder="N°de contacto" aria-label="Número de contacto" required/>
+                                <input type="text" className="form-control" placeholder="N°de contacto" aria-label="Número de contacto" required />
                             </div>
                         </div>
                         {/* seleccion casa */}
@@ -96,7 +92,12 @@ const Direccion = () => {
                                 Guardar esta dirección como predeterminada
                             </label>
                         </div>
-                        
+                        {/* BOTON PARA validar */}
+                        <div className="boton-para-pagar p-3">
+                            <button type="submit" class="btn btn-primary btn-lg" >Validar dirección</button>
+
+                        </div>
+
                     </form>
                 </div>
             </div>
