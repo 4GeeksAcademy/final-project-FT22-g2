18cf65d3ea53766ef52cb63aa13e3473ff1bb495
Matías Rivas <87586371+Dageti@users.noreply.github.com>
2023-11-28 13:01:07 +0000

    corregido responsive gallery component

--- src/front/js/component/Gallery.jsx
+++ src/front/js/component/Gallery.jsx
@@ -4,14 +4,14 @@ import ModalImages from "./ModalImages.jsx";
 const Gallery = () => {
   return (
     <>
-      <div className="container">
-        <div class="lightbox">
-          <div class="row">
-            <div class="col-lg-6">
-              <div>
+      <div className="container-fluid container-lg text-center">
+        <div className="lightbox">
+          <div className="row gx-0">
+            <div className="col-lg-6 p-2">
+              <div className="align-content-end gx-0"> 
                 <button type="button" className="custom-btn" data-bs-toggle="modal" data-bs-target="#ModalImages">
                   <img
-                    className="img-top py-5 hoverimage"
+                    className="img-top img-fluid py-5 hoverimage"
                     src="https://dummyimage.com/600x300/000/fff"
                     alt="Top Left Image"
                   />
@@ -20,18 +20,18 @@ const Gallery = () => {
               <div>
                 <button type="button" className="custom-btn" data-bs-toggle="modal" data-bs-target="#ModalImages">
                 <img
-                  className="img-top hoverimage"
+                  className="img-top img-fluid hoverimage"
                   src="https://dummyimage.com/600x300/000/fff"
                   alt="Bottom Left Image"
                 />
                 </button>
               </div>
             </div>
-            <div class="col-lg-6">
+            <div className=" p-2 col-lg-6">
             <div>
                 <button type="button" className="custom-btn" data-bs-toggle="modal" data-bs-target="#ModalImages">
                 <img
-                  className="img-top py-5 "
+                  className="img-top img-fluid py-5 "
                   src="https://dummyimage.com/600x650/000/fff"
                   alt="Right Image"
                 />
