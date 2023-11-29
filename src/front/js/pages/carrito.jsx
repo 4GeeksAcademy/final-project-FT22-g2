75b7f4e17ad8583198d8219c5b96cff5d61a42a4
Jorge Cancino Oteíza <139603426+JorgeOteiza@users.noreply.github.com>
2023-11-29 14:40:19 +0000

    función lista sumar/restar productos carrito

--- src/front/js/pages/carrito.jsx
+++ src/front/js/pages/carrito.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Carrito = () => {
     const [productos, setProductos] = useState([
         // ... productos iniciales
-        { price: 5990 }, { price: 36790 }, { price: 14870 }, { price: 16980 } //ejemplos provisorios
+        { price: 5990, cantidad: 1 }, { price: 36790, cantidad: 1 }, { price: 14870, cantidad: 1 }, { price: 16980, cantidad: 1 } //ejemplos provisorios
     ]);
 
     const deleteProduct = (index) => {
@@ -14,6 +14,20 @@ const Carrito = () => {
         const nuevosProductos = [...productos];
         nuevosProductos.splice(index, 1);
         setProductos(nuevosProductos); // Actualiza el estado con la nueva lista de productos
+    };
+
+    const incrementarCantidad = (index) => {
+        const nuevosProductos = [...productos];
+        nuevosProductos[index].cantidad++;
+        setProductos(nuevosProductos);
+    };
+
+    const decrementarCantidad = (index) => {
+        const nuevosProductos = [...productos];
+        if (nuevosProductos[index].cantidad > 1) {
+            nuevosProductos[index].cantidad--;
+            setProductos(nuevosProductos);
+        }
     };
 
     return (
@@ -34,15 +48,15 @@ const Carrito = () => {
                             <div className="col-6 d-flex justify-content-center">
                                 {/* BOTON AÑADIR Y QUITAR + PRECIO REUTILIZADO */}
                                 <div className="product-price-carrito-hover d-inline-flex align-items-center justify-content-between">
-                                    <button type='button' className="button-add-remove-carrito-hover remove-carrito-hover">-</button>
-                                    <label className="label-cantidad-carrito-hover px-2">1</label>
-                                    <button type='button' className="button-add-remove-carrito-hover add-carrito-hover ">+</button>
+                                    <button onClick={() => decrementarCantidad(index)} type='button' className="button-add-remove-carrito-hover remove-carrito-hover">-</button>
+                                    <label className="label-cantidad-carrito-hover px-2">{producto.cantidad}</label>
+                                    <button onClick={() => incrementarCantidad(index)} type='button' className="button-add-remove-carrito-hover add-carrito-hover">+</button>
                                 </div>
                             </div>
                             <div className="col-2 columna-vista-carrito-valor columna-vista-carrito-precio-articulo">
-                                <h2>${producto.price}</h2>
+                                <h2>${producto.price * producto.cantidad}</h2>
                             </div>
-                            <button onClick={() => deleteProduct(index)} className="col-1 columna-vista-carrito-valor icon-eliminar-producto-carrito"><i class="icon-hover-eliminar-producto fa-regular fa-trash-can"></i></button>
+                            <button onClick={() => deleteProduct(index)} className="col-1 columna-vista-carrito-valor icon-eliminar-producto-carrito"><i className="icon-hover-eliminar-producto fa-regular fa-trash-can"></i></button>
                         </div>
                     </div>
                 ))}
@@ -58,13 +72,13 @@ const Carrito = () => {
                                 <h5>Subtotal</h5>
                             </div>
                             <div className="col-6 ps-2">
-                                <h5>$74.630</h5>
+                                <h5>${productos.reduce((total, prod) => total + prod.price * prod.cantidad, 0)}</h5>
                             </div>
                             <div className="col-6 text-end">
                                 <h4>Total</h4>
                             </div>
                             <div className="col-6 ps-2">
-                                <h5>$74.630</h5>
+                                <h5>${productos.reduce((total, prod) => total + prod.price * prod.cantidad, 0)}</h5>
                             </div>
                             <Link to="/detalles-pedido" type="button" href="#" className="col-12 btn-confirmar-pedido-carrito text-decoration-none text-center">
                                 Confirmar
