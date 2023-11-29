a48fcade87b6979aa06bd21b02c82e6b31992f1b
Matías Rivas <87586371+Dageti@users.noreply.github.com>
2023-11-29 22:58:41 +0000

    cambios a descartar

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,11 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-<<<<<<< HEAD
-from api.models import db, User 
-=======
 from api.models import db, User, Producto
->>>>>>> dffccd75b51971351598f2cab09836fcf7aae7c7
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -23,8 +19,7 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
-<<<<<<< HEAD
-=======
+
 
 # Rutas para la tabla User
 @api.route('/users', methods=['GET', 'POST'])
@@ -45,4 +40,4 @@ def get_all_products():
     productos = Producto.query.all()
     return jsonify([producto.serialize() for producto in productos])
 
->>>>>>> dffccd75b51971351598f2cab09836fcf7aae7c7
+
