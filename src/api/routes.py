67db16909986d6efeaac348c28f91cfa399a405b
Matías Rivas <87586371+Dageti@users.noreply.github.com>
2023-11-30 15:12:01 +0000

    corrigiendo error

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,1 +1,86 @@
-, { useState, useEffect }
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+from flask import Flask, request, jsonify, url_for, Blueprint
+from api.models import db, User, Producto
+from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+from flask_cors import CORS
+
+api = Blueprint('api', __name__)
+CORS(api, resources={r"/api/": {"origins": "https://didactic-happiness-7qx694qjp792xjqj-3001.app.github.dev/", "methods": ["GET", "POST", "PUT", "DELETE"]}})
+
+# Allow CORS requests to this API
+CORS(api)
+
+@api.route('/hello', methods=['POST', 'GET'])
+def handle_hello():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
+
+# Rutas para la tabla User
+@api.route('/users', methods=['GET', 'POST'])
+def manage_users():
+    if request.method == 'GET':
+        users = User.query.all()
+        return jsonify([user.username for user in users])
+    elif request.method == 'POST':
+        data = request.json
+        new_user = User(username=data['username'], email=data['email'], 
+                        active=data['active'], password=generate_password_hash(data['password']))
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'Usuario creado exitosamente'}), 201
+
+@api.route('/productos', methods=['GET'])
+def get_all_products():
+    productos = Producto.query.all()
+    return jsonify([producto.serialize() for producto in productos])
+
+@api.route('/login', methods=['POST', 'GET'])
+def login():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email or not password:
+        return jsonify({'message': 'Email and password are required'}), 400
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({'message': 'Invalid email or password'}), 401
+    
+    access_token = create_access_token(identity={'email': user.email})
+    return jsonify({'access_token': access_token, 'message': 'Login successful'}), 200
+
+@api.route('/productos/<int:producto_id>', methods=['GET', 'PUT', 'DELETE'])
+def producto_detail(producto_id):
+    producto = Producto.query.get_or_404(producto_id)
+    if request.method == 'GET':
+        return jsonify(
+            {
+            "id": producto.id,
+            "nombre": producto.nombre,
+            "categoria": producto.categoria,
+            "tipo": producto.tipo,
+            "unitFormat": producto.unitFormat,
+            "precio": producto.precio,
+            "active": producto.active,
+            "image": producto.image,
+             }
+            )
+
+@api.route('/users/<int:user_id>', methods=['GET', 'DELETE'])
+def user_detail(user_id):
+    user = User.query.get_or_404(user_id)
+    if request.method == 'GET':
+        return jsonify({'username': user.username, 'email': user.email, 'active': user.active})
+    elif request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'Usuario eliminado exitosamente'})
