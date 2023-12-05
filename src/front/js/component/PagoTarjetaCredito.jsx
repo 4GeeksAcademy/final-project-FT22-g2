824ebd8c58bb108895b43bcf278f725385ca8be1
Demian Sotomayor <122990398+Demian-Sotomayor@users.noreply.github.com>
2023-12-05 20:04:46 +0000

    Ahora si se ve lindo metodo de paago con credito

--- src/front/js/component/PagoTarjetaCredito.jsx
+++ src/front/js/component/PagoTarjetaCredito.jsx
@@ -4,6 +4,7 @@ import 'react-credit-cards/es/styles-compiled.css'
 
 import pagoTarjetaDeCredito from "../../styles/pagoTarjetaDeCredito.css";
 import { Link } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 const TarjetaDeCredito = () => {
 
@@ -23,46 +24,84 @@ const TarjetaDeCredito = () => {
         let isValid = true;
 
         if (state.number === '' || state.name === '' || state.expiry === '' || state.cvc === '') {
-            alert("Todos los campos son obligatorios");
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Todos los campos son obligatorios!",
+            });
         } else {
 
             if (!numberOnly.test(state.number)) {
-                alert('El número de tarjeta sólo puede contener dígitos');
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "El número de tarjeta sólo puede contener dígitos!",
+                });
                 isValid = false;
             }
 
             if (state.number.length < 16 || state.number.length > 16) {
-                alert('Número de tarjeta debe tener 16 dígitos');
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Número de tarjeta debe tener 16 dígitos!",
+                });
                 isValid = false;
             }
 
             if (state.name.length < 1 || state.name.length > 40) {
-                alert('El nombre no puede ir vacío');
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "El nombre no puede ir vacío!",
+                });
                 isValid = false;
             }
 
             if (!numberOnly.test(state.expiry)) {
-                alert('La fecha de expiración sólo puede contener dígitos');
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "La fecha de expiración sólo puede contener dígitos!",
+                });
                 isValid = false;
             }
 
             if (state.expiry.length < 4 || state.expiry.length > 4) {
-                alert('Fecha de expiración debe tener 4 dígitos');
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Fecha de expiración debe tener 4 dígitos!",
+                });
                 isValid = false;
             }
 
             if (!numberOnly.test(state.cvc)) {
-                alert('El CVC sólo puede contener dígitos');
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "El CVC sólo puede contener dígitos!",
+                });
                 isValid = false;
             }
 
             if (state.cvc.length < 3 || state.cvc.length > 3) {
-                alert('CVC debe tener 3 dígitos');
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "CVC debe tener 3 dígitos!",
+                });
                 isValid = false;
             }
 
             if (isValid) {
-                alert("Los campos se rellenaron correctamente");
+                Swal.fire({
+                    icon: "success",
+                    title: "Los campos se rellenaron correctamente!",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
                 setTimeout(() => {
                     window.location.href = process.env.BASENAME + "metodo-de-pago/direccion";
                 }, 2000);
