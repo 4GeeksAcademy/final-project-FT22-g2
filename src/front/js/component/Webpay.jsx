e8f008b2b1817682050f56fc242ae21fe9cff577
Demian Sotomayor <122990398+Demian-Sotomayor@users.noreply.github.com>
2023-12-05 20:15:08 +0000

    Metodo de pago webpay listo

--- src/front/js/component/Webpay.jsx
+++ src/front/js/component/Webpay.jsx
@@ -1,39 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
 import pagoTarjetaDeCredito from "../../styles/pagoTarjetaDeCredito.css";
 import webpayLogo from "../../img/webpayLogo.png"
 import Direccion from "./Direccion.jsx";
-
+import Swal from 'sweetalert2'
 
 const Webpay = () => {
 
-    const pagar = () => {
-        let timerInterval;
-        Swal.fire({
-            icon: "success",
-            title: "¡Gracias por su compra!",
-            imageUrl: logoElRinconDelVino,
-            imageWidth: 250,
-            imageHeight: 180,
-            imageAlt: "Custom image",
-            html: "Será redirigido a la página principal en <b></b> milisegundos.",
-            timer: 7000,
-            timerProgressBar: true,
-            didOpen: () => {
-                Swal.showLoading();
-                const timer = Swal.getPopup().querySelector("b");
-                timerInterval = setInterval(() => {
-                    timer.textContent = `${Swal.getTimerLeft()}`;
-                }, 100);
-            },
-            willClose: () => {
-                clearInterval(timerInterval);
+    const [state, setState] = useState({
+        number: "",
+        name: "",
+        expiry: "",
+        cvc: ""
+    })
+
+    const handleOnClickDefault = (e) => {
+        e.preventDefault();
+
+        const numberOnly = /^\d+$/;
+
+        let isValid = true;
+
+        if (state.number === '' || state.name === '' || state.expiry === '' || state.cvc === '') {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Todos los campos son obligatorios!",
+            });
+        } else {
+
+            if (!numberOnly.test(state.number)) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "El número de tarjeta sólo puede contener dígitos!",
+                });
+                isValid = false;
             }
-        }).then((result) => {
-            if (result.dismiss === Swal.DismissReason.timer) {
-                console.log("I was closed by the timer");
-                window.location.href = "/";
+
+            if (state.number.length < 16 || state.number.length > 16) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Número de tarjeta debe tener 16 dígitos!",
+                });
+                isValid = false;
             }
-        });
+
+            if (state.name.length < 1 || state.name.length > 40) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "El nombre no puede ir vacío!",
+                });
+                isValid = false;
+            }
+
+            if (!numberOnly.test(state.expiry)) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "La fecha de expiración sólo puede contener dígitos!",
+                });
+                isValid = false;
+            }
+
+            if (state.expiry.length < 4 || state.expiry.length > 4) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Fecha de expiración debe tener 4 dígitos!",
+                });
+                isValid = false;
+            }
+
+            if (!numberOnly.test(state.cvc)) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "El CVC sólo puede contener dígitos!",
+                });
+                isValid = false;
+            }
+
+            if (state.cvc.length < 3 || state.cvc.length > 3) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "CVC debe tener 3 dígitos!",
+                });
+                isValid = false;
+            }
+
+            if (isValid) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Los campos se rellenaron correctamente!",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+                setTimeout(() => {
+                    window.location.href = process.env.BASENAME + "metodo-de-pago/direccion";
+                }, 2000);
+            }
+        }
+    }
+
+    const handleInputChange = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleFocusChange = (e) => {
+        setState({
+            ...state,
+            focus: e.target.name
+        })
     }
 
     return (
@@ -66,9 +150,12 @@ const Webpay = () => {
                                 type="text"
                                 name="number"
                                 id="number-tarjeta"
-                                maxLength="16"
+                                maxLength={16}
+                                minLength={16}
                                 className="form-control"
                                 required
+                                onChange={handleInputChange}
+                                onFocus={handleFocusChange}
                             />
                         </div>
                         {/*  nombre tarjeta */}
@@ -78,9 +165,11 @@ const Webpay = () => {
                                 type="text"
                                 name="name"
                                 id="name-tarjeta"
-                                maxLength="40"
+                                maxLength={40}
                                 className="form-control"
                                 required
+                                onChange={handleInputChange}
+                                onFocus={handleFocusChange}
                             />
                         </div>
                         {/* fecha de expiracion tarjeta */}
@@ -91,9 +180,12 @@ const Webpay = () => {
                                     type="text"
                                     name="expiry"
                                     id="expiryTarjeta"
-                                    maxLength="4"
+                                    maxLength={4}
+                                    minLength={4}
                                     className="form-control"
                                     required
+                                    onChange={handleInputChange}
+                                    onFocus={handleFocusChange}
                                 />
 
                             </div>
@@ -104,16 +196,19 @@ const Webpay = () => {
                                     type="text"
                                     name="cvc"
                                     id="cvcTarjeta"
-                                    maxLength="4"
+                                    maxLength={3}
+                                    minLength={3}
                                     className="form-control"
                                     required
+                                    onChange={handleInputChange}
+                                    onFocus={handleFocusChange}
                                 />
                             </div>
                         </div>
 
                         {/* BOTON PARA validar */}
                         <div className="boton-para-cambiarDireccion p-3">
-                            <button onClick={pagar} type="submit" className="btn btn-secondary btn-sm" >Validar tarjeta</button>
+                            <button onClick={handleOnClickDefault} type="submit" className="btn btn-secondary btn-sm">Continuar</button>
                         </div>
 
                     </form>
